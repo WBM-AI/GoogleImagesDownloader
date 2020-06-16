839947218d9cbb685bd3449f0b983f4b907ad200
Catalina Chircu <35842763+Catadanna@users.noreply.github.com>
2020-06-16 14:41:36 +0200

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,32 @@
+MAIN_KEYWORDS = ['school', 'scoala']
+SUP_KEYWORDS = ['école']
+MAX_NUM_IMAGES = 1000
+
+'''
+MAIN_KEYWORDS = ['neutral', 'angry', 'surprise', 'disgust', 'fear', 'happy', 'sad']
+
+SUP_KEYWORDS = ['facial expression',\
+                'human face',\
+                'face',\
+                'old face',\
+                'young face',\
+                'adult face',\
+                'child face',\
+                'woman face',\
+                'man face',\
+                'male face',\
+                'female face',\
+                'gentleman face',\
+                'lady face',\
+                'boy face',\
+                'girl face',\
+                'American face',\
+                'Chinese face',\
+                'Korean face',\
+                'Japanese face',\
+                'actor face',\
+                'actress face'\
+                'doctor face',\
+                'movie face'
+                ]
+'''
